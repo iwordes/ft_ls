038660243781c3226320dcd10f568054815a4a95
Ian Wordes <iwordes@student.42.us.org>
2017-01-11 12:26:17 -0800

    Potentially improve illegal option handling

--- src/err_illegal_opt.c
+++ src/err_illegal_opt.c
@@ -6,16 +6,15 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 14:20:45 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/10 15:25:23 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/11 12:26:00 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 
-void	err_illegal_opt(const char *bin, char opt)
+void	err_illegal_opt(char opt)
 {
-	ft_printf("%s: illegal option -- %c\n", bin, opt);
-	ft_printf("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1]"
-				" [file ...]\n");
+	ft_printf("%s: illegal option -- %c\n", g_name, opt);
+	ft_printf("usage: ls [-AGRSUacilrtu1] [file ...]\n");
 	exit(1);
 }
