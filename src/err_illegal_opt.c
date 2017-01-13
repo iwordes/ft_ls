ea5bfb7f318b5e27216ff9146737c645955ad5f7
Ian Wordes <iwordes@student.42.us.org>
2017-01-13 14:01:50 -0800

    Repair(?) err_illegal_opt

--- src/err_illegal_opt.c
+++ src/err_illegal_opt.c
@@ -6,15 +6,15 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 14:20:45 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/11 12:26:00 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/13 10:14:32 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 
-void	err_illegal_opt(char opt)
+void	err_illegal_opt(const char *path, char opt)
 {
-	ft_printf("%s: illegal option -- %c\n", g_name, opt);
+	ft_printf("%s: illegal option -- %c\n", path, opt);
 	ft_printf("usage: ls [-AGRSUacilrtu1] [file ...]\n");
 	exit(1);
 }
