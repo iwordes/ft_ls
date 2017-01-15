b4fc775a16491d200a7bb84327bce09da913bb11
Ian Wordes <iwordes@student.42.us.org>
2017-01-15 14:27:59 -0800

    Fix issue with off-by-one-year errors

--- src/ls_fmt_timeyear.c
+++ src/ls_fmt_timeyear.c
@@ -6,13 +6,25 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 09:18:52 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/08 13:10:35 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/14 11:04:23 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
-#define SEC_IN_YEAR 31556926
+#define SEC_IN_YEAR /*(86400 * 365)*/ 31556926
 #define SEC_IN_6MO (SEC_IN_YEAR / 2)
+
+static void	year_(char buffer[20], char *stamp)
+{
+	size_t	i;
+
+	while (stamp[0] == ' ')
+		stamp += 1;
+	i = ~0;
+	while (ft_isdigit(stamp[i += 1]))
+		buffer[i] = stamp[i];
+	buffer[i] = 0;
+}
 
 const char	*ls_fmt_timeyear(time_t epoch)
 {
@@ -23,7 +35,7 @@ const char	*ls_fmt_timeyear(time_t epoch)
 	if (epoch > now || now - epoch > SEC_IN_6MO)
 	{
 		buffer[0] = ' ';
-		ls__naitoa(buffer + 1, (epoch / SEC_IN_YEAR) + 1970);
+		year_(buffer + 1, ctime(&epoch) + 20);
 	}
 	else
 	{
